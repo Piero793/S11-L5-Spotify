49fc98c97259a614385c57beb02ed1b896e1021a
“Piero793” <pierolacitignola7@gmail.com>
2025-02-21 11:02:44 +0100

    MainSection created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Sidebar from "./components/SideBar";
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <MainSection />
     </>
   );
 }
