6fcaa6155aa94f9491b57c2275510be69e95f23a
“Piero793” <pierolacitignola7@gmail.com>
2025-02-21 10:56:34 +0100

    first settings and sidebar created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Sidebar from "./components/SideBar";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
