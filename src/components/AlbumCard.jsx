590c40ccdcc43d3c1254ed97c23413e09ba4430c
“Piero793” <pierolacitignola7@gmail.com>
2025-02-21 11:17:42 +0100

    component AlbumCard created

---
+++ src/components/AlbumCard.jsx
@@ -0,0 +1,13 @@
+const AlbumCard = ({ singleSong }) => {
+  return (
+    <div className="col text-center">
+      <img className="img-fluid" src={singleSong.album.cover_medium} alt="track" />
+      <p>
+        Track: "{singleSong.title}"<br />
+        Artist: {singleSong.artist.name}
+      </p>
+    </div>
+  );
+};
+
+export default AlbumCard;
