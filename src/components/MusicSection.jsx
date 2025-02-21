c743cdec16858c6300378b348d7d901e2c8c2f70
“Piero793” <pierolacitignola7@gmail.com>
2025-02-21 14:29:15 +0100

    updating file

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -11,10 +11,10 @@ const MusicSection = ({ artistName, sectionId }) => {
         const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistName}`);
         if (response.ok) {
           const { data } = await response.json();
-          console.log("API response:", data); // Verifica la struttura dei dati restituiti
+          console.log("DATI:", data);
           setSongs(data.slice(0, 4));
         } else {
-          throw new Error("Error in fetching songs");
+          throw new Error("Errore nella fetch");
         }
       } catch (err) {
         console.log("error", err);
@@ -22,8 +22,6 @@ const MusicSection = ({ artistName, sectionId }) => {
     };
     fetchSongs();
   }, [artistName]);
-
-  console.log("songs:", songs); // Verifica i dati passati
 
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3" id={sectionId}>
