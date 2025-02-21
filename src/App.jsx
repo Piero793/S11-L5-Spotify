1988a83695640bdf0b98ce148435a2fff790a6b3
“Piero793” <pierolacitignola7@gmail.com>
2025-02-21 11:10:28 +0100

    Player created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Sidebar from "./components/SideBar";
 import MainSection from "./components/MainSection";
+import Player from "./components/Player";
 
 function App() {
   return (
     <>
       <Sidebar />
       <MainSection />
+      <Player />
     </>
   );
 }
